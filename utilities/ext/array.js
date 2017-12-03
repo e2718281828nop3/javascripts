3639190d6a09eab6da5f9e92f5735fcabc508915
Masato Ishimoto <masato.ishimoto@gmail.com>
2017-12-03 21:33:48 +0900

    change header

--- utilities/ext/array.js
+++ utilities/ext/array.js
@@ -1,9 +1,9 @@
 // @name Array extension
 // @version 0.1
-// @author e2.718281828.nop3
-// @copyright 2017, e2.718281828.nop3
+// @author e2718281828nop3
+// @copyright 2017, e2718281828nop3(https://github.com/e2718281828nop3/)
 // @license GPLv3
-// @namespace https://openuserjs.org/users/e2.718281828.nop3
+// @namespace https://github.com/e2718281828nop3/
 // @exclude *
 Object.defineProperties(Array.prototype, {
     'first': {
